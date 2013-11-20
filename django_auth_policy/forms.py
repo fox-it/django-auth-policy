fc238eafd5e5fa649a3af3b25903e5c0e2561d66
David Mora <dla.mora@gmail.com>
2013-11-20 16:18:44 -0600

    importing OrderedDict (python 2.7 structure) from ordereddict (2.6 port) if it fails to import.

--- django_auth_policy/forms.py
+++ django_auth_policy/forms.py
@@ -1,5 +1,9 @@
 import logging
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    # python 2.6 or earlier, use backport
+    from ordereddict import OrderedDict
 
 from django import forms
 from django.utils.translation import ugettext as _
