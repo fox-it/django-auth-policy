8b67e7ae27e56c30b6bca2980e6418702154e053
Ronald Evers <evers@fox-it.com>
2013-11-21 10:22:17 +0100

    only install ordereddict in py < 2.7 and test for py26

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup
+
+
+install_requires = ['Django>=1.5']
+if sys.hexversion < 0x02070000:
+    install_requires.append('ordereddict>=1.1')
+
 
 setup(
     name='django-auth-policy',
@@ -10,7 +17,7 @@ setup(
     author='Fox-IT B.V.',
     license='BSD',
     packages=['django_auth_policy'],
-    install_requires=['Django>=1.5'],
+    install_requires=install_requires,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
